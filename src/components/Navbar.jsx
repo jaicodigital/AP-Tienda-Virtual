61f730fa27bbaca3ae2f2acc460dd306fb12e32a
Jairo Colón <jaicodigital@gmail.com>
2022-01-18 20:40:30 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,47 +1,89 @@
-import React from 'react';
-import styled from 'styled-components';
-import SearchIcon from '@mui/icons-material/Search';
+import React from "react";
+import styled from "styled-components";
+import SearchIcon from "@mui/icons-material/Search";
+import Badge from "@mui/material/Badge";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 
 const Container = styled.div`
-height: 100px;
+  height: 100px;
 `;
 
 const Wrapper = styled.div`
-padding: 10px 20px;
-display: flex;
-justify-content: space-between;
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
 `;
 
 const Lenguage = styled.span`
-font-size: 14px;
-cursor: pointer;
+  font-size: 14px;
+  cursor: pointer;
 `;
 
 const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
 
-`; 
+const Input = styled.input`
+  border: none;
+`;
 
-const Left = styled.div`
-flex: 1;
-`
 const Center = styled.div`
-flex: 1;
-`
-const Right = styled.div`
-flex: 1;
-`
+  flex: 1;
+  text-align: center;
+`;
 
-export default function Navbar () {
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;    
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+`;
+
+export default function Navbar() {
   return (
-     <Container>
-         <Wrapper>
-             <Left>
-               <Lenguage>EN</Lenguage>
-               <SearchContainer><SearchIcon/></SearchContainer>
-             </Left>
-             <Center>Center</Center>
-             <Right>Right</Right>
-         </Wrapper>          
-     </Container>
-  ) 
-};
+    <Container>
+      <Wrapper>
+        <Left>
+          <Lenguage>EN</Lenguage>
+          <SearchContainer>
+            <Input placeholder="Search" />
+            <SearchIcon style={{color: "gray", fontSize:16}} />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>AP Tienda Virtual</Logo>
+        </Center>
+        <Right>
+          <MenuItem>Registrar</MenuItem>
+          <MenuItem>Iniciar Sesión</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlinedIcon/>
+            </Badge>
+          </MenuItem>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+}
